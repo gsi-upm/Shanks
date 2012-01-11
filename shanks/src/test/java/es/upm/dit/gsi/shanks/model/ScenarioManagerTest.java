7962bca57382bef6e14268119544c983efa6014b
carrerabarroso@gmail.com <carrerabarroso@gmail.com@6c4d5907-d6a1-e102-47d4-e4e6a2087a27>
2012-01-11 12:32:46 +0000

    Empty tests
    git-svn-id: https://shanks-with-mason.googlecode.com/svn/trunk@122 6c4d5907-d6a1-e102-47d4-e4e6a2087a27

---
+++ shanks/src/test/java/es/upm/dit/gsi/shanks/model/ScenarioManagerTest.java
@@ -0,0 +1,54 @@
+/** es.upm.dit.gsi.shanks
+ 11/01/2012
+
+ */
+package es.upm.dit.gsi.shanks.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author a.carrera
+ *
+ */
+public class ScenarioManagerTest {
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+    }
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+    }
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}

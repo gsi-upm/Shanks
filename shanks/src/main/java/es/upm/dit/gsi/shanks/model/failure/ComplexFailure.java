d6cd645fddc6ba56fe466cee96d1fe044a56c44f
carrerabarroso@gmail.com <carrerabarroso@gmail.com@6c4d5907-d6a1-e102-47d4-e4e6a2087a27>
2012-01-11 12:19:27 +0000

    Non stable version at all - DANGEROUS -> Commited for refactoring and clearing code
    git-svn-id: https://shanks-with-mason.googlecode.com/svn/trunk@119 6c4d5907-d6a1-e102-47d4-e4e6a2087a27

---
+++ shanks/src/main/java/es/upm/dit/gsi/shanks/model/failure/ComplexFailure.java
@@ -0,0 +1,26 @@
+/** es.upm.dit.gsi.shanks
+ 11/01/2012
+
+ */
+package es.upm.dit.gsi.shanks.model.failure;
+
+/**
+ * @author a.carrera
+ *
+ */
+public class ComplexFailure extends Failure {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = -1713104617728419692L;
+
+    /**
+     * @param type
+     */
+    public ComplexFailure(String type) {
+        super(type);
+        // TODO Auto-generated constructor stub
+    }
+
+}

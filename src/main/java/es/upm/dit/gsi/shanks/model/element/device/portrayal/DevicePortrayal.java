d4e83833569e1e49cd87e7dade95efb2a9a5bea7
carrerabarroso@gmail.com <carrerabarroso@gmail.com@6c4d5907-d6a1-e102-47d4-e4e6a2087a27>
2012-01-13 15:41:57 +0000

    git-svn-id: https://shanks-with-mason.googlecode.com/svn/trunk/shanks-core@147 6c4d5907-d6a1-e102-47d4-e4e6a2087a27

--- src/main/java/es/upm/dit/gsi/shanks/model/element/device/portrayal/DevicePortrayal.java
+++ src/main/java/es/upm/dit/gsi/shanks/model/element/device/portrayal/DevicePortrayal.java
@@ -2,18 +2,11 @@ package es.upm.dit.gsi.shanks.model.element.device.portrayal;
 
 import java.util.logging.Logger;
 
-import javax.media.j3d.Appearance;
-import javax.media.j3d.ColoringAttributes;
-import javax.media.j3d.Material;
 import javax.media.j3d.TransformGroup;
 import javax.vecmath.Color3f;
 
 import sim.portrayal3d.SimplePortrayal3D;
-
-import com.sun.j3d.utils.geometry.Sphere;
-
 import es.upm.dit.gsi.shanks.model.element.device.Device;
-import es.upm.dit.gsi.shanks.model.element.device.DeviceDefinitions;
 
 public class DevicePortrayal extends SimplePortrayal3D {
 
@@ -38,42 +31,42 @@ public class DevicePortrayal extends SimplePortrayal3D {
 
     @Override
     public TransformGroup getModel(Object obj, TransformGroup j3dModel) {
-        if (j3dModel == null) { //TODO I don't know if this check is required...
-            j3dModel = new TransformGroup();
-            Sphere s = new Sphere(diameter);
-            if (((Device) obj).getCurrentStatus() == DeviceDefinitions.HEALTHY_STATUS) {
-                j3dModel.removeAllChildren();
-                Appearance appearance = new Appearance();
-                appearance.setColoringAttributes(new ColoringAttributes(
-                        healthyColor, ColoringAttributes.SHADE_GOURAUD));
-                Material m = new Material();
-                m.setAmbientColor(healthyColor);
-                m.setEmissiveColor(0f, 0f, 0f);
-                m.setDiffuseColor(healthyColor);
-                m.setSpecularColor(1f, 1f, 1f);
-                m.setShininess(128f);
-                appearance.setMaterial(m);
-                s.setAppearance(appearance);
-                j3dModel.addChild(s);
-
-                clearPickableFlags(j3dModel);
-            } else {
-                j3dModel.removeAllChildren();
-                Appearance appearance = new Appearance();
-                appearance.setColoringAttributes(new ColoringAttributes(
-                        brokenColor, ColoringAttributes.SHADE_GOURAUD));
-                Material m = new Material();
-                m.setAmbientColor(brokenColor);
-                m.setEmissiveColor(0f, 0f, 0f);
-                m.setDiffuseColor(brokenColor);
-                m.setSpecularColor(1f, 1f, 1f);
-                m.setShininess(128f);
-                appearance.setMaterial(m);
-                s.setAppearance(appearance);
-                j3dModel.addChild(s);
-                clearPickableFlags(j3dModel);
-            }
-        }
+//        if (j3dModel == null) { //TODO I don't know if this check is required...
+//            j3dModel = new TransformGroup();
+//            Sphere s = new Sphere(diameter);
+//            if (((Device) obj).getCurrentStatus() == DeviceDefinitions.HEALTHY_STATUS) {
+//                j3dModel.removeAllChildren();
+//                Appearance appearance = new Appearance();
+//                appearance.setColoringAttributes(new ColoringAttributes(
+//                        healthyColor, ColoringAttributes.SHADE_GOURAUD));
+//                Material m = new Material();
+//                m.setAmbientColor(healthyColor);
+//                m.setEmissiveColor(0f, 0f, 0f);
+//                m.setDiffuseColor(healthyColor);
+//                m.setSpecularColor(1f, 1f, 1f);
+//                m.setShininess(128f);
+//                appearance.setMaterial(m);
+//                s.setAppearance(appearance);
+//                j3dModel.addChild(s);
+//
+//                clearPickableFlags(j3dModel);
+//            } else {
+//                j3dModel.removeAllChildren();
+//                Appearance appearance = new Appearance();
+//                appearance.setColoringAttributes(new ColoringAttributes(
+//                        brokenColor, ColoringAttributes.SHADE_GOURAUD));
+//                Material m = new Material();
+//                m.setAmbientColor(brokenColor);
+//                m.setEmissiveColor(0f, 0f, 0f);
+//                m.setDiffuseColor(brokenColor);
+//                m.setSpecularColor(1f, 1f, 1f);
+//                m.setShininess(128f);
+//                appearance.setMaterial(m);
+//                s.setAppearance(appearance);
+//                j3dModel.addChild(s);
+//                clearPickableFlags(j3dModel);
+//            }
+//        }
         return super.getModel(obj, j3dModel);
     }
 

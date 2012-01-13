3e21dc37d70b60c80bed0e46e1e41c76d2e6c0e8
carrerabarroso@gmail.com <carrerabarroso@gmail.com@6c4d5907-d6a1-e102-47d4-e4e6a2087a27>
2012-01-13 16:02:33 +0000

    git-svn-id: https://shanks-with-mason.googlecode.com/svn/trunk@152 6c4d5907-d6a1-e102-47d4-e4e6a2087a27

--- shanks-core/src/main/java/es/upm/dit/gsi/shanks/model/element/link/Link.java
+++ shanks-core/src/main/java/es/upm/dit/gsi/shanks/model/element/link/Link.java
@@ -1,6 +1,8 @@
 package es.upm.dit.gsi.shanks.model.element.link;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
 import es.upm.dit.gsi.shanks.model.element.NetworkElement;
 import es.upm.dit.gsi.shanks.model.element.device.Device;
@@ -11,21 +13,55 @@ import es.upm.dit.gsi.shanks.model.element.device.Device;
  * This class represents the links than connect the different devices
  * 
  * @author Daniel Lara
- * @version 0.1
+ * @author a.carrera
+ * @version 0.2
  * 
  */
 
 public class Link extends NetworkElement {
+    
+    private Logger logger = Logger.getLogger(Link.class.getName());
+    
     private List<Device> linkedDevices;
+    private int deviceCapacity;
 
-    public Link(String id) {
+    /**
+     * @param id
+     * @param capacity
+     */
+    public Link(String id, int capacity) {
         super(id);
+        this.deviceCapacity = capacity;
+        this.linkedDevices = new ArrayList<Device>();
     }
 
+    /**
+     * @return
+     */
     public List<Device> getLinkedDevices() {
         return linkedDevices;
     }
 
-    //TODO add devices
+    /**
+     * @param device
+     * @return
+     */
+    public boolean connectDevice(Device device) {
+        if (this.linkedDevices.size()<deviceCapacity) {
+            this.linkedDevices.add(device);
+            logger.fine("Link " + this.getID() + " has Device " + device.getID() + " in its linked device list.");
+            return true;
+        } else {
+            logger.fine("Link " + this.getID() + " is full of its capacity. Device " + device.getID() + " was not included in its linked device list.");
+            return false;
+        }
+    }
+    
+    /**
+     * @param device
+     */
+    public void disconnectDevice(Device device) {
+        this.linkedDevices.remove(device);
+    }
     
 }

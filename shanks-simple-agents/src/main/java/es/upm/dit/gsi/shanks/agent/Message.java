3397aabdf292305b5e48f0752b26afbddd50f668
carrerabarroso@gmail.com <carrerabarroso@gmail.com@6c4d5907-d6a1-e102-47d4-e4e6a2087a27>
2012-01-13 15:31:31 +0000

    Initial commit of simple agents module.
    git-svn-id: https://shanks-with-mason.googlecode.com/svn/trunk@141 6c4d5907-d6a1-e102-47d4-e4e6a2087a27

---
+++ shanks-simple-agents/src/main/java/es/upm/dit/gsi/shanks/agent/Message.java
@@ -0,0 +1,28 @@
+package es.upm.dit.gsi.shanks.agent;
+
+import es.upm.dit.gsi.shanks.model.element.device.Device;
+
+/**
+ * Message class
+ * 
+ * This class represent the messages send between the agents (it isn't in use
+ * yet)
+ * 
+ * @author Daniel Lara
+ * @version 0.1
+ * 
+ */
+
+// This class is not in use yet
+public class Message {
+    private Device sender;
+
+    public Message(Device sender) {
+        this.sender = sender;
+    }
+
+    public Device getSender() {
+        return sender;
+    }
+
+}

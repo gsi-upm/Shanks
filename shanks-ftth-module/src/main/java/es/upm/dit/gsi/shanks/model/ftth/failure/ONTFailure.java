12f5ff080b9f2367c9f6f6d9b85193877a73d569
carrerabarroso@gmail.com <carrerabarroso@gmail.com@6c4d5907-d6a1-e102-47d4-e4e6a2087a27>
2012-01-13 15:24:39 +0000

    Initial commit of ftth module
    git-svn-id: https://shanks-with-mason.googlecode.com/svn/trunk@137 6c4d5907-d6a1-e102-47d4-e4e6a2087a27

---
+++ shanks-ftth-module/src/main/java/es/upm/dit/gsi/shanks/model/ftth/failure/ONTFailure.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package es.upm.dit.gsi.shanks.model.ftth.failure;
+
+import es.upm.dit.gsi.shanks.model.failure.ConfigurationFailure;
+import es.upm.dit.gsi.shanks.model.failure.FailureDefinitions;
+
+/**
+ * @author a.carrera
+ * 
+ */
+public class ONTFailure extends ConfigurationFailure {
+    // LOOK una clase como esta es la que deben implementar para cada fallo
+    /**
+	 * 
+	 */
+    private static final long serialVersionUID = 7899469300086259485L;
+
+    /**
+     * @param type
+     */
+    public ONTFailure() {
+        super(FailureDefinitions.FAILURE_CONFIGURATION_ONT);
+    }
+
+}

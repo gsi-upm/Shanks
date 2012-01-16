9be4a4ff4d5a2278703811fabe66ca01bc185b36
carrerabarroso@gmail.com <carrerabarroso@gmail.com@6c4d5907-d6a1-e102-47d4-e4e6a2087a27>
2012-01-16 13:50:13 +0000

    git-svn-id: https://shanks-with-mason.googlecode.com/svn/trunk@160 6c4d5907-d6a1-e102-47d4-e4e6a2087a27

--- shanks-core/src/main/java/es/upm/dit/gsi/shanks/model/failure/NetworkFailure.java
+++ shanks-core/src/main/java/es/upm/dit/gsi/shanks/model/failure/NetworkFailure.java
@@ -6,11 +6,6 @@ package es.upm.dit.gsi.shanks.model.failure;
  */
 public abstract class NetworkFailure extends Failure {
 
-    /**
-	 * I think this class it is not required
-	 */
-    private static final long serialVersionUID = -838331953844650206L;
-
     public NetworkFailure(String type) {
         super(type);
     }

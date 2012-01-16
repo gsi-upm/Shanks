f8ab7b3ab9fdc3b6738f59cf6d6a7b4697621424
carrerabarroso@gmail.com <carrerabarroso@gmail.com@6c4d5907-d6a1-e102-47d4-e4e6a2087a27>
2012-01-16 11:33:22 +0000

    git-svn-id: https://shanks-with-mason.googlecode.com/svn/trunk@156 6c4d5907-d6a1-e102-47d4-e4e6a2087a27

--- shanks-core/src/main/java/es/upm/dit/gsi/shanks/model/element/NetworkElement.java
+++ shanks-core/src/main/java/es/upm/dit/gsi/shanks/model/element/NetworkElement.java
@@ -8,6 +8,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.logging.Logger;
 
+import es.upm.dit.gsi.shanks.model.element.exception.UnsupportedNetworkElementStatusException;
+
 
 /**
  * @author a.carrera

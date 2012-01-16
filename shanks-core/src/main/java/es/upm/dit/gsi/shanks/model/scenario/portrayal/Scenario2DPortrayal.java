a713282d803281b1a1e793480fb4cc4703c5f7ad
carrerabarroso@gmail.com <carrerabarroso@gmail.com@6c4d5907-d6a1-e102-47d4-e4e6a2087a27>
2012-01-16 16:33:26 +0000

    git-svn-id: https://shanks-with-mason.googlecode.com/svn/trunk@167 6c4d5907-d6a1-e102-47d4-e4e6a2087a27

--- shanks-core/src/main/java/es/upm/dit/gsi/shanks/model/scenario/portrayal/Scenario2DPortrayal.java
+++ shanks-core/src/main/java/es/upm/dit/gsi/shanks/model/scenario/portrayal/Scenario2DPortrayal.java
@@ -1,25 +1,33 @@
 package es.upm.dit.gsi.shanks.model.scenario.portrayal;
 
+import sim.field.grid.SparseGrid2D;
 import sim.portrayal.grid.SparseGridPortrayal2D;
 import sim.portrayal.network.NetworkPortrayal2D;
-import sim.util.Double3D;
+import sim.util.Int2D;
 import es.upm.dit.gsi.shanks.model.element.device.Device;
 
 /**
  * @author a.carrera
- *
+ * 
  */
 public abstract class Scenario2DPortrayal {
 
     private int gridWidth;
     private int gridHeight;
-    
-    private SparseGridPortrayal2D elements = new SparseGridPortrayal2D();
-    private NetworkPortrayal2D links = new NetworkPortrayal2D();
-    private SparseGridPortrayal2D failures = new SparseGridPortrayal2D();
 
-    public void situateDevice(Device d, double x, double y) {
-        elements.setObjectLocation(d, new Double3D(x, y, z));
-        devicesnetwork.add(d);
+    private SparseGrid2D elements;
+    private NetworkPortrayal2D links;
+    private SparseGridPortrayal2D failures;
+
+    public Scenario2DPortrayal(int width, int height) {
+        this.gridHeight = height;
+        this.gridWidth = width;
+        elements = new SparseGrid2D(width, height);
+        links = new NetworkPortrayal2D();
+        failures = new SparseGridPortrayal2D();
+    }
+
+    public void situateDevice(Device d, int x, int y) {
+        elements.setObjectLocation(d, new Int2D(x, y));
     }
 }

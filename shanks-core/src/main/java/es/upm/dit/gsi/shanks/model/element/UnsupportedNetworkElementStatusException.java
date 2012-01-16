8374402e08800aa10e1b07867990ecb8895023d3
carrerabarroso@gmail.com <carrerabarroso@gmail.com@6c4d5907-d6a1-e102-47d4-e4e6a2087a27>
2012-01-16 11:32:50 +0000

    git-svn-id: https://shanks-with-mason.googlecode.com/svn/trunk@155 6c4d5907-d6a1-e102-47d4-e4e6a2087a27

---
+++ shanks-core/src/main/java/es/upm/dit/gsi/shanks/model/element/UnsupportedNetworkElementStatusException.java
@@ -0,0 +1,14 @@
+package es.upm.dit.gsi.shanks.model.element;
+
+public class UnsupportedNetworkElementStatusException extends Exception {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 8739490073386103417L;
+
+    public UnsupportedNetworkElementStatusException() {
+        super("The status is not supported by the Network Element.");
+    }
+    
+}

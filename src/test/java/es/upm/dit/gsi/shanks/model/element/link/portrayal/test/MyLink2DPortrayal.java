99a0ba34db78461af8d1cfb9bd1eb9837aa6a612
carrerabarroso@gmail.com <carrerabarroso@gmail.com@6c4d5907-d6a1-e102-47d4-e4e6a2087a27>
2012-01-18 17:58:07 +0000

    git-svn-id: https://shanks-with-mason.googlecode.com/svn/trunk/shanks-core@197 6c4d5907-d6a1-e102-47d4-e4e6a2087a27

--- src/test/java/es/upm/dit/gsi/shanks/model/element/link/portrayal/test/MyLink2DPortrayal.java
+++ src/test/java/es/upm/dit/gsi/shanks/model/element/link/portrayal/test/MyLink2DPortrayal.java
@@ -17,10 +17,55 @@ public class MyLink2DPortrayal extends Link2DPortrayal {
      */
     private static final long serialVersionUID = -7612525235009977268L;
 
-    
     public void draw(Object object, Graphics2D graphics, DrawInfo2D info) {
-        EdgeDrawInfo2D ei = (EdgeDrawInfo2D) info;
         Edge e = (Edge) object;
+
+        Link link = (Link) e.getInfo();
+        if (link.getCapacity() == 2) {
+            this.drawSimpleLink(link, object, graphics, info);
+        } else if (link.getCapacity() > 2) {
+//            this.drawSimpleLink(link, object, graphics, info);
+            this.drawComplexLink(link, object, graphics, info);
+        }
+
+    }
+
+    private void drawComplexLink(Link link, Object object, Graphics2D graphics,
+            DrawInfo2D info) {
+
+        EdgeDrawInfo2D ei = (EdgeDrawInfo2D) info;
+        final double width = 20;
+        final double height = 20;
+
+        String status = link.getCurrentStatus();
+
+        graphics.setColor(Color.black);
+        if (status.equals(MyLink.OK)) {
+            graphics.setColor(Color.green);
+        } else if (status.equals(MyLink.BROKEN)) {
+            graphics.setColor(Color.red);
+        }
+
+
+        final int startX = (int) ei.draw.x;
+        final int startY = (int) ei.draw.y;
+        final int endX = (int) ei.secondPoint.x;
+        final int endY = (int) ei.secondPoint.y;
+        final int midX = (int) (ei.draw.x + ei.secondPoint.x) / 2;
+        final int midY = (int) (ei.draw.y + ei.secondPoint.y) / 2;
+        final int w = (int) (width);
+        final int h = (int) (height);
+        graphics.drawLine(startX, startY, endX, endY);
+        graphics.fillRect(midX-(int)(width/2), midY-(int)(height/2), w, h);
+
+        // Draw the devices ID ID
+        graphics.setColor(Color.black);
+        graphics.drawString(link.getID(), midX - 5, midY);
+    }
+
+    private void drawSimpleLink(Link link, Object object, Graphics2D graphics,
+            DrawInfo2D info) {
+        EdgeDrawInfo2D ei = (EdgeDrawInfo2D) info;
 
         final int startX = (int) ei.draw.x;
         final int startY = (int) ei.draw.y;
@@ -29,22 +74,19 @@ public class MyLink2DPortrayal extends Link2DPortrayal {
         final int midX = (int) (ei.draw.x + ei.secondPoint.x) / 2;
         final int midY = (int) (ei.draw.y + ei.secondPoint.y) / 2;
 
-        Link link = (Link) e.getInfo();
-
-        graphics.setColor(Color.green);
+        graphics.setColor(Color.black);
         String status = link.getCurrentStatus();
         if (status.equals(MyLink.OK)) {
             graphics.setColor(Color.green);
         } else if (status.equals(MyLink.BROKEN)) {
             graphics.setColor(Color.red);
-        }        
+        }
         graphics.drawLine(startX, startY, endX, endY);
-        
+
         graphics.setColor(Color.blue);
         graphics.setFont(labelFont);
         int width = graphics.getFontMetrics().stringWidth(link.getID());
-        graphics.drawString( link.getID(), midX - width / 2, midY );
-
+        graphics.drawString(link.getID(), midX - width / 2, midY);
     }
 
 }

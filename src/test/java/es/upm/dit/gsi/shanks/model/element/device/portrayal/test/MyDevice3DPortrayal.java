19fb42ecb5e47c790ac92e92f6ed65e66352da51
carrerabarroso@gmail.com <carrerabarroso@gmail.com@6c4d5907-d6a1-e102-47d4-e4e6a2087a27>
2012-01-19 14:08:11 +0000

    3D Portrayal working with labels in devices and links :)
    git-svn-id: https://shanks-with-mason.googlecode.com/svn/trunk/shanks-core@201 6c4d5907-d6a1-e102-47d4-e4e6a2087a27

--- src/test/java/es/upm/dit/gsi/shanks/model/element/device/portrayal/test/MyDevice3DPortrayal.java
+++ src/test/java/es/upm/dit/gsi/shanks/model/element/device/portrayal/test/MyDevice3DPortrayal.java
@@ -1,12 +1,18 @@
 package es.upm.dit.gsi.shanks.model.element.device.portrayal.test;
 
 import java.awt.Color;
+import java.awt.Font;
 
 import javax.media.j3d.Appearance;
 import javax.media.j3d.ColoringAttributes;
 import javax.media.j3d.Material;
+import javax.media.j3d.OrientedShape3D;
+import javax.media.j3d.Shape3D;
+import javax.media.j3d.Transform3D;
 import javax.media.j3d.TransformGroup;
 import javax.vecmath.Color3f;
+import javax.vecmath.Point3f;
+import javax.vecmath.Vector3d;
 
 import com.sun.j3d.utils.geometry.Sphere;
 
@@ -15,6 +21,8 @@ import es.upm.dit.gsi.shanks.model.element.device.test.MyDevice;
 
 public class MyDevice3DPortrayal extends Device3DPortrayal {
 
+    
+    private int diameter = 50;
     /**
      * 
      */
@@ -26,17 +34,13 @@ public class MyDevice3DPortrayal extends Device3DPortrayal {
         model = new TransformGroup();
 
         Color3f colour;
-        Sphere s = new Sphere(50);
-        String status = device.getCurrentStatus();
-        if (status.equals(MyDevice.OK_STATUS)) {
-            colour = new Color3f(Color.green);
-        } else if (status.equals(MyDevice.NOK_STATUS)) {
-            colour = new Color3f(Color.red);
-        } else if (status.equals(MyDevice.UNKOWN_STATUS)) {
-            colour = new Color3f(Color.gray);
-        } else {
-            colour = new Color3f(Color.black);
-        }
+        Sphere s = new Sphere(diameter);
+        colour = new Color3f(this.getTargetColor(device));
+
+        int FONT_SIZE = 48;
+        Font labelFont = new Font("SansSerif", Font.BOLD, FONT_SIZE);
+        double labelScale = 1.0;
+        double SCALING_MODIFIER = 1.0 / 5.0; 
 
         model.removeAllChildren();
         Appearance appearance = new Appearance();
@@ -53,8 +57,77 @@ public class MyDevice3DPortrayal extends Device3DPortrayal {
         model.addChild(s);
         model.setName(device.getID());
         clearPickableFlags(model);
+        model.setPickable(true);
+        
+        
+
+
+        // draw the device labels if the user wants
+        Transform3D offset = new Transform3D();
+        offset.setTranslation(new Vector3d(5+diameter/2,5+diameter/2,5+diameter/2));
+        Transform3D trans = offset;
+        String str = device.getID();
+        com.sun.j3d.utils.geometry.Text2D text = new com.sun.j3d.utils.geometry.Text2D(
+                str, new Color3f(this.getLabelColor(device)), labelFont.getFamily(),
+                labelFont.getSize(), labelFont.getStyle());
+        text.setRectangleScaleFactor((float) (labelScale * SCALING_MODIFIER));
+
+        // text = new Shape3D(new Text3D(labelFont3D, ""));
+
+        OrientedShape3D o3d = new OrientedShape3D(text.getGeometry(),
+                text.getAppearance(), OrientedShape3D.ROTATE_ABOUT_POINT,
+                new Point3f(0, 0, 0));
+        o3d.setCapability(Shape3D.ALLOW_APPEARANCE_WRITE); // may need to change
+                                                           // the appearance
+                                                           // (see below)
+        o3d.setCapability(Shape3D.ALLOW_GEOMETRY_WRITE); // may need to change
+                                                         // the geometry (see
+                                                         // below)
+        o3d.clearCapabilityIsFrequent(Shape3D.ALLOW_APPEARANCE_WRITE);
+        o3d.clearCapabilityIsFrequent(Shape3D.ALLOW_GEOMETRY_WRITE);
+
+        // make the offset TransformGroup
+        TransformGroup o = new TransformGroup();
+        o.setCapability(TransformGroup.ALLOW_CHILDREN_READ);
+        o.setCapability(TransformGroup.ALLOW_TRANSFORM_READ);
+        o.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
+        o.clearCapabilityIsFrequent(TransformGroup.ALLOW_CHILDREN_READ);
+        o.setTransform(trans);
+        o.setUserData(str);
+
+        // the label shouldn't be pickable -- we'll turn this off in the
+        // TransformGroup
+        clearPickableFlags(o);
+        o.addChild(o3d); // Add label to the offset TransformGroup
+        model.addChild(o);
 
         return model;
+    }
+
+    private Color getTargetColor(MyDevice device) {
+        String status = device.getCurrentStatus();
+        if (status.equals(MyDevice.OK_STATUS)) {
+            return Color.green;
+        } else if (status.equals(MyDevice.NOK_STATUS)) {
+            return Color.red;
+        } else if (status.equals(MyDevice.UNKOWN_STATUS)) {
+            return Color.gray;
+        } else {
+            return Color.black;
+        }
+    }
+
+    private Color getLabelColor(MyDevice device) {
+        String status = device.getCurrentStatus();
+        if (status.equals(MyDevice.OK_STATUS)) {
+            return Color.blue;
+        } else if (status.equals(MyDevice.NOK_STATUS)) {
+            return Color.red;
+        } else if (status.equals(MyDevice.UNKOWN_STATUS)) {
+            return Color.gray;
+        } else {
+            return Color.black;
+        }
     }
 
 }

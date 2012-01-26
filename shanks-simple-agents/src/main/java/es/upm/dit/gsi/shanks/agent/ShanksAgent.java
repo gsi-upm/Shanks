4810185aa570f4ff63793d4ddbec28dede4626b5
carrerabarroso@gmail.com <carrerabarroso@gmail.com@6c4d5907-d6a1-e102-47d4-e4e6a2087a27>
2012-01-26 15:04:41 +0000

    git-svn-id: https://shanks-with-mason.googlecode.com/svn/trunk@278 6c4d5907-d6a1-e102-47d4-e4e6a2087a27

--- shanks-simple-agents/src/main/java/es/upm/dit/gsi/shanks/agent/ShanksAgent.java
+++ shanks-simple-agents/src/main/java/es/upm/dit/gsi/shanks/agent/ShanksAgent.java
@@ -2,31 +2,85 @@ package es.upm.dit.gsi.shanks.agent;
 
 import jason.architecture.AgArch;
 import jason.asSemantics.ActionExec;
+import jason.asSemantics.Message;
 import jason.asSyntax.Literal;
 
 import java.util.List;
+
+import es.upm.dit.gsi.shanks.ShanksSimulation;
 
 import sim.engine.SimState;
 import sim.engine.Steppable;
 import sim.engine.Stoppable;
 
-public abstract class ShanksAgent extends AgArch implements Steppable, Stoppable {
+/**
+ * @author a.carrera
+ *
+ */
+public abstract class ShanksAgent extends AgArch implements Steppable,
+		Stoppable {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 4744430503147830611L;
 
-	abstract public void step(SimState paramSimState);
-	abstract public void stop();
+	private ShanksSimulation simulation;
 
-	abstract public List<Literal> percieve();
-
-	public void act(ActionExec action, List<ActionExec> feedback) {
-		//TOIMP this method
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see sim.engine.Steppable#step(sim.engine.SimState)
+	 */
+	@Override
+	public void step(SimState simulation) {
+		this.simulation = (ShanksSimulation) simulation;
+		this.getTS().reasoningCycle();
 	}
-	
-	//TOIMP sendMessage method
-	//TOIMP checkMail method
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see jason.architecture.AgArch#stop()
+	 */
+	public void stop() {
+
+	}
+
+	/**
+	 * @return The simulation to get data
+	 */
+	public ShanksSimulation getSimulation() {
+		return simulation;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see jason.architecture.AgArch#perceive()
+	 */
+	abstract public List<Literal> perceive();
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see jason.architecture.AgArch#act(jason.asSemantics.ActionExec,
+	 * java.util.List)
+	 */
+	abstract public void act(ActionExec action, List<ActionExec> feedback);
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see jason.architecture.AgArch#checkMail()
+	 */
+	abstract public void checkMail();
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see jason.architecture.AgArch#sendMsg(jason.asSemantics.Message)
+	 */
+	abstract public void sendMsg(Message message);
 
 }

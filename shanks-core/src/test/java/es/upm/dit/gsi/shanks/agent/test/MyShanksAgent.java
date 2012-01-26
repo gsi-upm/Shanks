29f89c4596a4a0ecb2de105464e75f678aa141d7
carrerabarroso@gmail.com <carrerabarroso@gmail.com@6c4d5907-d6a1-e102-47d4-e4e6a2087a27>
2012-01-26 15:15:49 +0000

    Adding shanks agents
    git-svn-id: https://shanks-with-mason.googlecode.com/svn/trunk@281 6c4d5907-d6a1-e102-47d4-e4e6a2087a27

---
+++ shanks-core/src/test/java/es/upm/dit/gsi/shanks/agent/test/MyShanksAgent.java
@@ -0,0 +1,42 @@
+package es.upm.dit.gsi.shanks.agent.test;
+
+import jason.asSemantics.ActionExec;
+import jason.asSemantics.Message;
+import jason.asSyntax.Literal;
+
+import java.util.List;
+
+import es.upm.dit.gsi.shanks.agent.ShanksAgent;
+
+public class MyShanksAgent extends ShanksAgent {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2187089758395179991L;
+
+	@Override
+	public List<Literal> perceive() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void act(ActionExec action, List<ActionExec> feedback) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void checkMail() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void sendMsg(Message message) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

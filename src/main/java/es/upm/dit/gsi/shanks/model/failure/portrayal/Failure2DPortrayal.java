b940287a4ea00a8d0af097b04b212e2ef4c70693
carrerabarroso@gmail.com <carrerabarroso@gmail.com@6c4d5907-d6a1-e102-47d4-e4e6a2087a27>
2012-02-02 14:04:42 +0000

    Failure Portrayals :)
    git-svn-id: https://shanks-with-mason.googlecode.com/svn/trunk/shanks-core@302 6c4d5907-d6a1-e102-47d4-e4e6a2087a27

--- src/main/java/es/upm/dit/gsi/shanks/model/failure/portrayal/Failure2DPortrayal.java
+++ src/main/java/es/upm/dit/gsi/shanks/model/failure/portrayal/Failure2DPortrayal.java
@@ -8,7 +8,7 @@ import es.upm.dit.gsi.shanks.model.failure.Failure;
 import sim.portrayal.DrawInfo2D;
 import sim.portrayal.SimplePortrayal2D;
 
-public class FailurePortrayal extends SimplePortrayal2D {
+public class Failure2DPortrayal extends SimplePortrayal2D {
 
     /**
      * 
@@ -24,14 +24,24 @@ public class FailurePortrayal extends SimplePortrayal2D {
     @Override
     public void draw(Object object, Graphics2D graphics, DrawInfo2D info) {
 
-        Failure failure = (Failure) object;
-        int x = (int) info.draw.x;
-        int y = (int) info.draw.y;
+        Failure failure = (Failure) object;;
+        final double width = 5;
+        final double height = 5;
+
+        // Draw the devices
+        int x = (int) (info.draw.x - width / 2.0);
+        int y = (int) (info.draw.y - height / 2.0);
+        int w = (int) (width);
+        int h = (int) (height);        
+        graphics.setColor(Color.gray);
+        graphics.fillOval(x, y-2, w, h);
+        x = (int) ((int) info.draw.x + (width / 2.0));
+        y = (int) info.draw.y;
 
         // Draw the devices ID ID
         graphics.setColor(Color.black);
         graphics.drawString("Failure ID ----> "
-                + failure.getID(), x, y);
+                + failure.getID(), x+3, y+3);
     }
 
 }

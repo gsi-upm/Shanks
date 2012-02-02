9f461f797c9b1dbcb5ef935c1e8964687c53a4c9
carrerabarroso@gmail.com <carrerabarroso@gmail.com@6c4d5907-d6a1-e102-47d4-e4e6a2087a27>
2012-02-02 15:20:55 +0000

    Final complex scenario
    git-svn-id: https://shanks-with-mason.googlecode.com/svn/trunk/shanks-core@305 6c4d5907-d6a1-e102-47d4-e4e6a2087a27

---
+++ src/test/java/es/upm/dit/gsi/shanks/model/scenario/portrayal/test/FinalComplexScenario2DPortrayal.java
@@ -0,0 +1,104 @@
+/**
+ * 
+ */
+package es.upm.dit.gsi.shanks.model.scenario.portrayal.test;
+
+import sim.field.grid.SparseGrid2D;
+import sim.portrayal.continuous.ContinuousPortrayal2D;
+import sim.portrayal.grid.SparseGridPortrayal2D;
+import sim.portrayal.network.NetworkPortrayal2D;
+import sim.util.Double2D;
+import es.upm.dit.gsi.shanks.model.element.device.Device;
+import es.upm.dit.gsi.shanks.model.element.device.portrayal.test.MyDevice2DPortrayal;
+import es.upm.dit.gsi.shanks.model.element.device.test.MyDevice;
+import es.upm.dit.gsi.shanks.model.element.link.Link;
+import es.upm.dit.gsi.shanks.model.element.link.portrayal.test.MyLink2DPortrayal;
+import es.upm.dit.gsi.shanks.model.failure.portrayal.Failure2DPortrayal;
+import es.upm.dit.gsi.shanks.model.failure.test.MyFailure;
+import es.upm.dit.gsi.shanks.model.scenario.ComplexScenario;
+import es.upm.dit.gsi.shanks.model.scenario.Scenario;
+import es.upm.dit.gsi.shanks.model.scenario.exception.ScenarioNotFoundException;
+import es.upm.dit.gsi.shanks.model.scenario.portrayal.ComplexScenario2DPortrayal;
+import es.upm.dit.gsi.shanks.model.scenario.portrayal.Scenario2DPortrayal;
+import es.upm.dit.gsi.shanks.model.scenario.portrayal.ScenarioPortrayal;
+import es.upm.dit.gsi.shanks.model.scenario.portrayal.ShanksMath;
+import es.upm.dit.gsi.shanks.model.scenario.portrayal.exception.DuplicatedPortrayalIDException;
+
+/**
+ * @author a.carrera
+ *
+ */
+public class FinalComplexScenario2DPortrayal extends ComplexScenario2DPortrayal {
+
+    /**
+     * @param scenario
+     * @param width
+     * @param height
+     * @throws DuplicatedPortrayalIDException
+     * @throws ScenarioNotFoundException
+     */
+    public FinalComplexScenario2DPortrayal(Scenario scenario, int width,
+            int height) throws DuplicatedPortrayalIDException, ScenarioNotFoundException {
+        super(scenario, width, height);
+    }
+
+    /* (non-Javadoc)
+     * @see es.upm.dit.gsi.shanks.model.scenario.portrayal.Scenario2DPortrayal#addPortrayals()
+     */
+    @Override
+    public void addPortrayals() {
+        SparseGrid2D failuresGrid = new SparseGrid2D(100, 100);
+        SparseGridPortrayal2D failuresPortrayal = new SparseGridPortrayal2D();
+        failuresPortrayal.setField(failuresGrid);
+        try {
+            this.addPortrayal(MyHyperComplexScenario2DPortrayal.FAILURE_DISPLAY_ID, MyHyperComplexScenario2DPortrayal.FAILURE_PORTRAYAL_ID, failuresPortrayal);
+        } catch (DuplicatedPortrayalIDException e) {            
+            e.printStackTrace();
+        }
+    }
+
+    /* (non-Javadoc)
+     * @see es.upm.dit.gsi.shanks.model.scenario.portrayal.Scenario2DPortrayal#placeElements()
+     */
+    @Override
+    public void placeElements() {
+        ComplexScenario cs = (ComplexScenario) this.getScenario();
+        this.situateDevice((Device)cs.getNetworkElement("FED1"), 500, 650);
+        this.drawLink((Link)cs.getNetworkElement("FEL1"));
+        this.drawLink((Link)cs.getNetworkElement("FEL2"));
+        this.drawLink((Link)cs.getNetworkElement("FEL3"));
+        this.drawLink((Link)cs.getNetworkElement("FEL4"));
+        this.drawLink((Link)cs.getNetworkElement("FEL5"));
+        this.drawLink((Link)cs.getNetworkElement("FEL6"));
+        this.drawLink((Link)cs.getNetworkElement("FEL7"));
+        this.drawLink((Link)cs.getNetworkElement("FEL8"));
+    }
+
+    /* (non-Javadoc)
+     * @see es.upm.dit.gsi.shanks.model.scenario.portrayal.ScenarioPortrayal#setupPortrayals()
+     */
+    @Override
+    public void setupPortrayals() {
+        ContinuousPortrayal2D devicePortrayal = (ContinuousPortrayal2D) this.getPortrayals().get(Scenario2DPortrayal.MAIN_DISPLAY_ID).get(ScenarioPortrayal.DEVICES_PORTRAYAL);
+        NetworkPortrayal2D networkPortrayal = (NetworkPortrayal2D) this.getPortrayals().get(Scenario2DPortrayal.MAIN_DISPLAY_ID).get(ScenarioPortrayal.LINKS_PORTRAYAL);
+        SparseGridPortrayal2D failuresPortrayal = (SparseGridPortrayal2D) this.getPortrayals().get(MyHyperComplexScenario2DPortrayal.FAILURE_DISPLAY_ID).get(MyHyperComplexScenario2DPortrayal.FAILURE_PORTRAYAL_ID);
+        devicePortrayal.setPortrayalForClass(MyDevice.class, new MyDevice2DPortrayal());
+        networkPortrayal.setPortrayalForAll(new MyLink2DPortrayal());
+        failuresPortrayal.setPortrayalForClass(MyFailure.class, new Failure2DPortrayal());
+    }
+
+    @Override
+    public void placeScenarios() throws DuplicatedPortrayalIDException,
+            ScenarioNotFoundException {
+        ComplexScenario cs = (ComplexScenario) this.getScenario();
+        this.situateScenario(cs.getScenario("HyperComplexScenario1"), new Double2D(0,0), ShanksMath.ANGLE_0, ShanksMath.ANGLE_0);
+        this.situateScenario(cs.getScenario("HyperComplexScenario2"), new Double2D(350,0), ShanksMath.ANGLE_0, ShanksMath.ANGLE_0);
+        this.situateScenario(cs.getScenario("HyperComplexScenario3"), new Double2D(700,0), ShanksMath.ANGLE_0, ShanksMath.ANGLE_0);
+        this.situateScenario(cs.getScenario("HyperComplexScenario4"), new Double2D(0,450), ShanksMath.ANGLE_0, ShanksMath.ANGLE_0);
+        this.situateScenario(cs.getScenario("HyperComplexScenario5"), new Double2D(700,450), ShanksMath.ANGLE_0, ShanksMath.ANGLE_0);
+        this.situateScenario(cs.getScenario("HyperComplexScenario6"), new Double2D(0,900), ShanksMath.ANGLE_0, ShanksMath.ANGLE_0);
+        this.situateScenario(cs.getScenario("HyperComplexScenario7"), new Double2D(350,900), ShanksMath.ANGLE_0, ShanksMath.ANGLE_0);
+        this.situateScenario(cs.getScenario("HyperComplexScenario8"), new Double2D(700,900), ShanksMath.ANGLE_0, ShanksMath.ANGLE_0);
+    }
+
+}

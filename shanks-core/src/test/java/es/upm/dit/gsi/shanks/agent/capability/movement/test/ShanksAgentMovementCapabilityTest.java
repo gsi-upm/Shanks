9f282d3e6c2aa8b9aa734d03a482c8d22a7d7f75
carrerabarroso@gmail.com <carrerabarroso@gmail.com@6c4d5907-d6a1-e102-47d4-e4e6a2087a27>
2012-03-09 08:44:29 +0000

    Empty test classes
    git-svn-id: https://shanks-with-mason.googlecode.com/svn/trunk@368 6c4d5907-d6a1-e102-47d4-e4e6a2087a27

---
+++ shanks-core/src/test/java/es/upm/dit/gsi/shanks/agent/capability/movement/test/ShanksAgentMovementCapabilityTest.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package es.upm.dit.gsi.shanks.agent.capability.movement.test;
+
+/**
+ * @author a.carrera
+ *
+ */
+public class ShanksAgentMovementCapabilityTest {
+
+    //TOTEST implement tests for this capability
+}
---
+++ shanks-core/src/test/java/es/upm/dit/gsi/shanks/agent/capability/perception/test/ShanksAgentPerceptionCapabilityTest.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package es.upm.dit.gsi.shanks.agent.capability.perception.test;
+
+/**
+ * @author a.carrera
+ * 
+ */
+public class ShanksAgentPerceptionCapabilityTest {
+
+    //TOTEST implement tests for this capability
+
+}

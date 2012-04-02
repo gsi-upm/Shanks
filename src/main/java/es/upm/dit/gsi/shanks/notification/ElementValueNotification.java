5feb924bbededf4c897ad2d551b69760c329e94c
darofar <darofar@6c4d5907-d6a1-e102-47d4-e4e6a2087a27>
2012-04-02 15:33:11 +0000

    Separation of notifications into two diferent types. Events and User-defined.
    git-svn-id: https://shanks-with-mason.googlecode.com/svn/trunk/shanks-core@616 6c4d5907-d6a1-e102-47d4-e4e6a2087a27

--- src/main/java/es/upm/dit/gsi/shanks/notification/ElementValueNotification.java
+++ src/main/java/es/upm/dit/gsi/shanks/notification/ElementValueNotification.java
@@ -4,10 +4,64 @@
 package es.upm.dit.gsi.shanks.notification;
 
 /**
+ * ElementValueNotifactions are user-made notifications that trace the value of certain 
+ * field, variable or whatever it wants to save for consult later. In this case  a 
+ * notification has the form:
  * 
- * @author Danny
+ *      source => elementID => value 
+ *      
+ * @author darofar
  *
  */
-public class ElementValueNotification {
+public class ElementValueNotification extends Notification {
+
+    /**
+     * 
+     */
+    private String elementID = null;
+    
+    /**
+     * Value 
+     */
+    private Object value;
+    
+    /**
+     * 
+     * @param id
+     * @param when
+     * @param source
+     */
+    public ElementValueNotification(String id, long when, Object source) {
+        super(id, when, source);
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @param id
+     * @param when
+     * @param source
+     * @param elementID
+     * @param value
+     */
+    public ElementValueNotification(String id, long when, Object source,
+            String elementID, Object value) {
+        super(id, when, source);
+        this.elementID = elementID;
+        this.value = value;
+    }
+
+    /**
+     * @return the element identifier. 
+     */
+    public String getElementID() {
+        return elementID;
+    }
+
+    /**
+     * @return the saved value of the element. 
+     */
+    public Object getValue() {
+        return value;
+    }
 
 }
---
+++ src/main/java/es/upm/dit/gsi/shanks/notification/InteractionNotification.java
@@ -0,0 +1,76 @@
+/**
+ * 
+ */
+package es.upm.dit.gsi.shanks.notification;
+
+import java.util.List;
+
+import es.upm.dit.gsi.shanks.model.event.OneShotEvent;
+import es.upm.dit.gsi.shanks.model.event.PeriodicEvent;
+import es.upm.dit.gsi.shanks.model.event.ProbabilisticEvent;
+
+/**
+ * A InteractionNotification store information of Events, cause in SHANKS all interactions
+ * between two elements are defined by them. This information have a relational
+ * entity with the form:
+ * 
+ *      source => interaction => target
+ * 
+ * @author darofar
+ *
+ */
+public class InteractionNotification extends Notification {
+    
+    public static final String ONE_SHOOT_EVENT_TYPE = OneShotEvent.class.getName();
+    public static final String PERIODIC_EVENT_TYPE = PeriodicEvent.class.getName();
+    public static final String PROBABILISTIC_EVENT_TYPE = ProbabilisticEvent.class.getName();
+    
+    /**
+     * Name of the class that define the interaction itself. 
+     */
+    private String interaction = null;
+    
+    /**
+     * Interaction target objects.
+     */
+    private List<Object> target = null;
+    
+    /**
+     * 
+     * @param id
+     * @param when
+     * @param source
+     */
+    public InteractionNotification(String id, long when, Object source) {
+        super(id, when, source);
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @param id
+     * @param when
+     * @param source
+     * @param interaction
+     * @param target
+     */
+    public InteractionNotification(String id, long when, Object source,
+            String interaction, List<Object> target) {
+        super(id, when, source);
+        this.interaction = interaction;
+        this.target = (List<Object>) target;
+    }
+
+    /**
+     * @return the name of the class that defines the  interaction.
+     */
+    public String getInteraction() {
+        return interaction;
+    }
+
+    /**
+     * @return a list of interaction's target objects 
+     */
+    public List<Object> getTarget() {
+        return target;
+    }
+}

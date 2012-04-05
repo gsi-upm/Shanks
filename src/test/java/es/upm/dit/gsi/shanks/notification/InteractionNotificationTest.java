80ae37ec4444bee573a918bdec330b7874df1f9f
darofar@gmail.com <darofar@gmail.com@6c4d5907-d6a1-e102-47d4-e4e6a2087a27>
2012-04-05 18:04:37 +0000

    Notifications JUnit 100%
    git-svn-id: https://shanks-with-mason.googlecode.com/svn/trunk/shanks-core@621 6c4d5907-d6a1-e102-47d4-e4e6a2087a27

--- src/test/java/es/upm/dit/gsi/shanks/notification/InteractionNotificationTest.java
+++ src/test/java/es/upm/dit/gsi/shanks/notification/InteractionNotificationTest.java
@@ -14,7 +14,12 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import es.upm.dit.gsi.shanks.model.event.OneShotEvent;
+import es.upm.dit.gsi.shanks.model.event.PeriodicEvent;
+import es.upm.dit.gsi.shanks.model.event.ProbabilisticEvent;
+import es.upm.dit.gsi.shanks.notification.test.TestAgent;
 import es.upm.dit.gsi.shanks.notification.test.TestDefinitions;
+import es.upm.dit.gsi.shanks.notification.test.TestDevice;
 
 public class InteractionNotificationTest {
 
@@ -50,14 +55,14 @@ public class InteractionNotificationTest {
     public void createNotification() {
 
         InteractionNotification iNotification = new InteractionNotification(
-                TestDefinitions.IN_ID, TestDefinitions.IN_WHEN,
-                TestDefinitions.IN_SOURCE, TestDefinitions.IN_INTERACTION,
+                TestDefinitions.NOTIFICATION_ID, TestDefinitions.NOTIFICATION_WHEN,
+                TestDefinitions.NOTIFICATION_SOURCE, TestDefinitions.IN_INTERACTION,
                 TestDefinitions.IN_TARGET);
 
-        Assert.assertTrue(iNotification.getId().equals(TestDefinitions.IN_ID));
-        Assert.assertTrue(iNotification.getWhen() == TestDefinitions.IN_WHEN);
+        Assert.assertTrue(iNotification.getId().equals(TestDefinitions.NOTIFICATION_ID));
+        Assert.assertTrue(iNotification.getWhen() == TestDefinitions.NOTIFICATION_WHEN);
         Assert.assertTrue(iNotification.getSource().equals(
-                TestDefinitions.IN_SOURCE));
+                TestDefinitions.NOTIFICATION_SOURCE));
         Assert.assertTrue(iNotification.getInteraction().equals(
                 TestDefinitions.IN_INTERACTION));
         Assert.assertTrue(iNotification.getTarget().equals(
@@ -73,8 +78,8 @@ public class InteractionNotificationTest {
         ArrayList<Notification> ln = new ArrayList<Notification>();
         
         for (int i = 0; i < TestDefinitions.NOTIFICATIONS_SIZE; i++) {
-            ln.add(new InteractionNotification(TestDefinitions.IN_ID+i, 
-                    TestDefinitions.IN_WHEN, TestDefinitions.IN_SOURCE, 
+            ln.add(new InteractionNotification(TestDefinitions.NOTIFICATION_ID+i, 
+                    TestDefinitions.NOTIFICATION_WHEN, TestDefinitions.NOTIFICATION_SOURCE, 
                     TestDefinitions.IN_INTERACTION, TestDefinitions.IN_TARGET));
         }
         
@@ -91,7 +96,7 @@ public class InteractionNotificationTest {
     
     /**
      * Create a list of notifications, add them to a NotificationManager and them 
-     * make queries to the NotificationManager. 
+     * make ID queries to the NotificationManager. 
      */
     @Test
     public void checkIDQueries() {
@@ -99,8 +104,8 @@ public class InteractionNotificationTest {
         ArrayList<Notification> ln = new ArrayList<Notification>();
         
         for (int i = 0; i < TestDefinitions.NOTIFICATIONS_SIZE; i++) {
-            ln.add(new InteractionNotification(TestDefinitions.IN_ID+i, 
-                    TestDefinitions.IN_WHEN, TestDefinitions.IN_SOURCE, 
+            ln.add(new InteractionNotification(TestDefinitions.NOTIFICATION_ID+i, 
+                    TestDefinitions.NOTIFICATION_WHEN, TestDefinitions.NOTIFICATION_SOURCE, 
                     TestDefinitions.IN_INTERACTION, TestDefinitions.IN_TARGET));
         }
         
@@ -121,7 +126,7 @@ public class InteractionNotificationTest {
     
     /**
      * Create a list of notifications, add them to a NotificationManager and them 
-     * make queries to the NotificationManager. 
+     * make step queries to the NotificationManager. 
      */
     @Test
     public void checkStepQueries() {
@@ -130,23 +135,21 @@ public class InteractionNotificationTest {
         HashMap<String, Integer> stepCount = new HashMap<String, Integer>();
         
         for (int i = 0; i < TestDefinitions.NOTIFICATIONS_SIZE; i++) {
-            Long step = Math.round(TestDefinitions.IN_WHEN*Math.random());
+            Long step = Math.round(TestDefinitions.NOTIFICATION_WHEN*Math.random());
             if(stepCount.containsKey(step.toString())){
                 stepCount.put(step.toString(), stepCount.get(step.toString())+1);
             } else {
                 stepCount.put(step.toString(), 1);
             }
-            ln.add(new InteractionNotification(TestDefinitions.IN_ID+i, step, 
-                    TestDefinitions.IN_SOURCE, TestDefinitions.IN_INTERACTION, 
+            ln.add(new InteractionNotification(TestDefinitions.NOTIFICATION_ID+i, step, 
+                    TestDefinitions.NOTIFICATION_SOURCE, TestDefinitions.IN_INTERACTION, 
                     TestDefinitions.IN_TARGET));
         }
         
-        ArrayList<Notification> obtained_ln = new ArrayList<Notification>();
         NotificationManager nm = null;
         try {
             nm = new NotificationManager(ln, null, TestDefinitions.getSimulation());
-            obtained_ln= nm.getByType(InteractionNotification.class);
-            Assert.assertEquals(ln, obtained_ln);
+            Assert.assertEquals(ln, nm.getByType(InteractionNotification.class));
             
             for(String step:stepCount.keySet()){
                 List<Notification> obtained = nm.getByStep(Integer.parseInt(step));
@@ -158,6 +161,215 @@ public class InteractionNotificationTest {
         } catch (Exception e) {
             e.printStackTrace();
             Assert.fail(""+e);
+        }
+    }
+    
+    /**
+     * Create a list of notifications, add them to a NotificationManager and them 
+     * make step queries to the NotificationManager. 
+     */
+    @Test
+    public void checkSourceQueries() {
+        
+        ArrayList<Notification> ln = new ArrayList<Notification>();
+        HashMap<Object, Integer> sourceCount = new HashMap<Object, Integer>();
+        ArrayList<Object> allSources = new ArrayList<Object>();
+        
+        for(int i=0; i<TestDefinitions.SOURCES_SIZE+1; i++)
+            allSources.add(new TestAgent(TestDefinitions.AGENT_ID+i));
+        
+        for (int i=0; i < TestDefinitions.NOTIFICATIONS_SIZE; i++) {
+            Object source = null;
+            switch((int)(Math.round(TestDefinitions.SOURCES_SIZE*Math.random()))) {
+            case 0:
+                source = allSources.get(0);
+                break;
+            case 1:
+                source = allSources.get(1);
+                break;    
+            case 2:
+                source = allSources.get(2);
+                break;
+            case 3:
+                source = allSources.get(3);
+                break;
+            case 4:
+                source = allSources.get(4);
+                break;
+            case 5:
+                source = allSources.get(5);
+                break;
+            default:
+                break;
+            }
+            if(sourceCount.containsKey(source)){
+                sourceCount.put(source, sourceCount.get(source)+1);
+            } else {
+                sourceCount.put(source, 1);
+            }
+            ln.add(new InteractionNotification(TestDefinitions.NOTIFICATION_ID+i,
+                    TestDefinitions.NOTIFICATION_WHEN, source, TestDefinitions.IN_INTERACTION, 
+                    TestDefinitions.IN_TARGET));
+        }
+        NotificationManager nm = null;
+        try {
+            nm = new NotificationManager(ln, null, TestDefinitions.getSimulation());
+            Assert.assertEquals(ln, nm.getByType(InteractionNotification.class));
+            
+            for(Object source:sourceCount.keySet()){
+                List<Notification> obtained = nm.getBySource(source);
+                Assert.assertTrue(obtained.size()==sourceCount.get(source).intValue());
+                for (Notification n: obtained){
+                    Assert.assertTrue(ln.contains(n));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            Assert.fail(""+e);
+        }
+    }
+    
+    /**
+     * Create a list of notifications, add them to a NotificationManager and them 
+     * make step queries to the NotificationManager. 
+     */
+    @Test
+    public void checkInteractionQueries() {
+        
+        ArrayList<Notification> ln = new ArrayList<Notification>();
+        HashMap<String, Integer> interactionCount = new HashMap<String, Integer>();
+        
+        for (int i = 0; i < TestDefinitions.NOTIFICATIONS_SIZE; i++) {
+            String interaction = null;
+            switch((int)(Math.round(2*Math.random()))) {
+            case 0:
+                interaction = OneShotEvent.class.getName();
+                break;
+            case 1:
+                interaction = PeriodicEvent.class.getName();
+                break;    
+            case 2:
+                interaction = ProbabilisticEvent.class.getName();
+                break;
+            }
+            if(interactionCount.containsKey(interaction)){
+                interactionCount.put(interaction, interactionCount.get(interaction)+1);
+            } else {
+                interactionCount.put(interaction, 1);
+            }
+            ln.add(new InteractionNotification(TestDefinitions.NOTIFICATION_ID+i, TestDefinitions.NOTIFICATION_WHEN,  
+                    TestDefinitions.NOTIFICATION_SOURCE, interaction, TestDefinitions.IN_TARGET));
+        }
+        
+        NotificationManager nm = null;
+        try {
+            nm = new NotificationManager(ln, null, TestDefinitions.getSimulation());
+            Assert.assertEquals(ln, nm.getByType(InteractionNotification.class));
+            
+            for(String interaction:interactionCount.keySet()){
+                List<Notification> obtained = nm.getByInteraction(interaction);
+                Assert.assertTrue(obtained.size()==interactionCount.get(interaction).intValue());
+                for (Notification n: obtained){
+                    Assert.assertTrue(ln.contains(n));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            Assert.fail(""+e);
+        }
+    }
+    
+    /**
+     * Create a list of notifications, add them to a NotificationManager and them 
+     * make queries by target to the NotificationManager. 
+     */
+    @Test
+    public void checkTargetQueries() {
+        
+        ArrayList<Notification> ln = new ArrayList<Notification>();
+        HashMap<Object, Integer> targetCount = new HashMap<Object, Integer>();
+        
+        try {
+            ArrayList<Object> allTargets = new ArrayList<Object>();
+            
+            for(int i=0; i<TestDefinitions.TARGETS_SIZE; i++)
+                allTargets.add(new TestDevice(TestDefinitions.DEVICE_ID+i, null, false));
+            
+            for (int i = 0; i < TestDefinitions.NOTIFICATIONS_SIZE; i++) {
+                ArrayList<Object> usedTargets = new ArrayList<Object>();
+                for (int j=0; j<TestDefinitions.TARGETS_SIZE; j++){
+                    Object tempTarget = null;
+                    switch((int)(Math.round((TestDefinitions.TARGETS_SIZE-1)*Math.random()))) {
+                    case 0:
+                        tempTarget = allTargets.get(0);
+                        break;
+                    case 1:
+                        tempTarget = allTargets.get(1);
+                        break;    
+                    case 2:
+                        tempTarget = allTargets.get(2);
+                        break;
+                    case 3:
+                        tempTarget = allTargets.get(3);
+                        break;
+                    case 4:
+                        tempTarget = allTargets.get(4);
+                        break;
+                    default:
+                        break;
+                    }
+                    if(targetCount.containsKey(tempTarget)){
+                        targetCount.put(tempTarget, targetCount.get(tempTarget)+1);
+                    } else {
+                        targetCount.put(tempTarget, 1);
+                    }
+                    usedTargets.add(tempTarget);
+                }
+                ln.add(new InteractionNotification(TestDefinitions.NOTIFICATION_ID+i,
+                        TestDefinitions.NOTIFICATION_WHEN, TestDefinitions.NOTIFICATION_SOURCE, 
+                        TestDefinitions.IN_INTERACTION, usedTargets));
+            }
+            
+            NotificationManager nm = null;
+            nm = new NotificationManager(ln, null, TestDefinitions.getSimulation());
+            Assert.assertEquals(ln, nm.getByType(InteractionNotification.class));
+            
+            for(Object target:targetCount.keySet()){
+                List<Notification> obtained = nm.getByTarget(target);
+                Assert.assertTrue(obtained.size()==targetCount.get(target).intValue());
+                for (Notification n: obtained){
+                    Assert.assertTrue(ln.contains(n));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            Assert.fail(""+e);
+        }
+        
+    }
+    
+    /**
+     * Create a list of notifications, add them to a NotificationManager and them 
+     * make queries by ElementId to the NotificationManager. 
+     */
+    @Test
+    public void checkElementIDQueries() {
+        ArrayList<Notification> ln = new ArrayList<Notification>();
+        
+        for (int i = 0; i < TestDefinitions.NOTIFICATIONS_SIZE; i++) {
+            ln.add(new InteractionNotification(TestDefinitions.NOTIFICATION_ID+i, 
+                    TestDefinitions.NOTIFICATION_WHEN, TestDefinitions.NOTIFICATION_SOURCE, 
+                    TestDefinitions.IN_INTERACTION, TestDefinitions.IN_TARGET));
+        }
+        try {
+            NotificationManager nm = new NotificationManager(ln, null, TestDefinitions.getSimulation());
+            Assert.assertEquals(ln, (nm.getByType(InteractionNotification.class)));
+            ln.add(new InteractionNotification(null, 0, nm, null, null));
+            Assert.assertNotSame(ln, (nm.getByType(InteractionNotification.class)));
+            List<Notification> obtained = nm.getByElementID(TestDefinitions.EVN_ELEMENT_ID);
+            Assert.assertEquals(null, obtained);
+            Assert.fail();
+        } catch (Exception e) {
         }
     }
     
--- src/test/java/es/upm/dit/gsi/shanks/notification/test/TestDefinitions.java
+++ src/test/java/es/upm/dit/gsi/shanks/notification/test/TestDefinitions.java
@@ -21,15 +21,25 @@ import es.upm.dit.gsi.shanks.model.test.MyShanksSimulation;
 
 public class TestDefinitions {
 
-    public static final String IN_ID = "Notification#";
-    public static final long IN_WHEN = 5;
-    public static final Object IN_SOURCE = new Object();
+    public static final String NOTIFICATION_ID = "Notification#";
+    public static final long NOTIFICATION_WHEN = 5;
+    public static final Object NOTIFICATION_SOURCE = new Object();
+    
     public static final String IN_INTERACTION = Event.class.getName();
-    public static final List<Object> IN_TARGET = new ArrayList();
+    public static final List<Object> IN_TARGET = new ArrayList<Object>();
+    
+    public static final String EVN_ELEMENT_ID = "ElementID";
+    public static final Object EVN_VALUE = new Object();
     
     public static final int NOTIFICATIONS_SIZE = 10;
+    public static final int SOURCES_SIZE = 5;
+    public static final int TARGETS_SIZE = 5;
+    public static final int ELEMENT_ID_SIZE = 5;
     
-    public static final String EVENT_ID = "testEvent";
+    public static final String EVENT_ID = "testEvent#";
+    public static final String AGENT_ID = "testAgent#";
+    public static final String DEVICE_ID = "testDevice#";
+
     
     /**
      * Accessory method that creates a test simulation. 

c358b2439fbeae8a037fc4929b5d4b6c0b2e1f07
darofar@gmail.com <darofar@gmail.com@6c4d5907-d6a1-e102-47d4-e4e6a2087a27>
2012-05-15 16:53:18 +0000

    Firewall+DNS+Redesign of total network.
    git-svn-id: https://shanks-with-mason.googlecode.com/svn/trunk@765 6c4d5907-d6a1-e102-47d4-e4e6a2087a27

--- shanks-enterprise-module/src/main/java/es/upm/dit/gsi/shanks/shanks_enterprise_module/model/element/CompanyRouter.java
+++ shanks-enterprise-module/src/main/java/es/upm/dit/gsi/shanks/shanks_enterprise_module/model/element/CompanyRouter.java
@@ -1,60 +1,13 @@
 package es.upm.dit.gsi.shanks.shanks_enterprise_module.model.element;
 
 import es.upm.dit.gsi.shanks.model.element.exception.UnsupportedNetworkElementStatusException;
-import es.upm.dit.gsi.shanks.model.scenario.Scenario;
 import es.upm.dit.gsi.shanks.networkattacks.util.networkelements.RouterDNS;
 
 public class CompanyRouter extends RouterDNS{
 
-	public static final String STATUS_OK = "Ok";
-	public static final String STATUS_OFF ="Off";
 	
-	public static final String PROPERTY_POWER = "Power";
-	public static final String PROPERTY_PORTS = "Ports";
-	
-	
-	private boolean[] ports;
-	
-	public CompanyRouter(String id, Scenario parent)
+	public CompanyRouter(String id)
 			throws UnsupportedNetworkElementStatusException {
-		super(id, parent);
-		this.initPorts();
+		super(id);
 	}
-
-	@Override
-	public void fillIntialProperties() {
-		this.addProperty(PROPERTY_POWER, true);
-		this.addProperty(PROPERTY_PORTS, ports);
-	}
-
-	@Override
-	public void checkStatus()
-			throws UnsupportedNetworkElementStatusException {
-		Boolean power = (Boolean) this.getProperty(PROPERTY_POWER);
-		if(power){
-			this.updateStatusTo(STATUS_OFF, false);
-			this.updateStatusTo(STATUS_OK, true);
-		}else{
-			this.updateStatusTo(STATUS_OFF, true);
-			this.updateStatusTo(STATUS_OK, false);
-		}
-	}
-
-	@Override
-	public void checkProperties() throws UnsupportedNetworkElementStatusException {
-	}
-
-	@Override
-	public void setPossibleStates() {
-		this.addPossibleStatus(STATUS_OFF);
-		this.addPossibleStatus(STATUS_OK);
-	}
-	
-	private void initPorts(){
-		this.ports = new boolean[1000];
-		for(int i = 0; i < ports.length; i++){
-			this.ports[i] = true;
-		}
-	}
-
 }

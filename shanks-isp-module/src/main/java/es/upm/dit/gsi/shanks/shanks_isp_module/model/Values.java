7a6824b42028b6db19b48291066b34c2e2770f75
mardoalmar@gmail.com <mardoalmar@gmail.com@6c4d5907-d6a1-e102-47d4-e4e6a2087a27>
2012-05-15 19:02:44 +0000

    Updated attacks and hacker
    git-svn-id: https://shanks-with-mason.googlecode.com/svn/trunk@767 6c4d5907-d6a1-e102-47d4-e4e6a2087a27

--- shanks-isp-module/src/main/java/es/upm/dit/gsi/shanks/shanks_isp_module/model/Values.java
+++ shanks-isp-module/src/main/java/es/upm/dit/gsi/shanks/shanks_isp_module/model/Values.java
@@ -10,10 +10,8 @@ public class Values extends es.upm.dit.gsi.shanks.networkattacks.util.Values {
 	
 	// HANS values. 
 	public static final String HAN_SUSCRIBER_LINK = "HanSuscription";
-	public static final String HAN_SCENARIO_ID = "HomeAreaNetwork#";
 	
 	// Enterpirse Values
-	public static final String ENTERPRISE_SCENARIO_ID = "MegaCorp";
 	public static final String ENTERPRISE_SUSCRIBER_LINK_ID = "MegaCorpSuscription";
 
 }
